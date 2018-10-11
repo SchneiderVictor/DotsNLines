e51e9434b2006b00eb2953a926f524c894b46ffd
schneider <schneider.victor@live.ca>
2018-10-10 23:18:39 -0400

    added javadoc to Dot.java

--- app/src/main/java/com/example/schne/dotsnlines/Dot.java
+++ app/src/main/java/com/example/schne/dotsnlines/Dot.java
@@ -2,18 +2,39 @@ package com.example.schne.dotsnlines;
 
 import java.util.ArrayList;
 
+/**
+ * A Dot, associated with a Button, that contains its valid neighbors.
+ */
 public class Dot {
-	private ArrayList<Dot> neighbours = new ArrayList<>();
+	// valid neighbours are either directly above, below, to the left or the right of *this* dot
+	private ArrayList<Dot> validNeighbours = new ArrayList<>();
 	
 	public Dot() {}
 	
+	/**
+	 * Record dot as a valid neighbor
+	 *
+	 * @param dot a valid neighbour
+	 */
 	public void addNeighbor(Dot dot) {
-		neighbours.add(dot);
+		validNeighbours.add(dot);
 	}
 	
+	/**
+	 * returns whether or not dot a valid neighbour.
+	 * If so, dot gets removed from *this'* validNeighbours list and
+	 * *this* dot gets removed from dot's validNeighbours list
+	 *
+	 * @param dot potential neighbour
+	 * @return true iff dot is a valid neighbour
+	 */
 	public boolean isValidNeighbor(Dot dot) {
-		if (neighbours.contains(dot)) {
-			neighbours.remove(dot);
+		if (validNeighbours.contains(dot)) {
+			
+			// both validNeighbors lists must be updated since the line between
+			// them cannot be placed once more
+			validNeighbours.remove(dot);
+			dot.validNeighbours.remove(this);
 			return true;
 		}
 		return false;
